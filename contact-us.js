2379e4332c630d16efed24a9edf5442da971cb05
vageog <65826917+vageog@users.noreply.github.com>
2020-07-06 17:18:09 +0300

    JS

--- contact-us.js
+++ contact-us.js
@@ -1,7 +1,7 @@
 var contactButton = document.querySelector(".contact-us-button");
 var contactWindow = document.querySelector(".contact-us-window");
 var contactForm = document.querySelector(".contact-us-fields");
-var contactClose = contactButton.querySelector(".close-button");
+var contactClose = document.querySelector(".close-button");
 var contactName = document.querySelector(".contact-us-name");
 var contactEmail = document.querySelector(".contact-us-email");
 var contactText = document.querySelector(".contact-us-text");
@@ -33,13 +33,7 @@ contactClose.addEventListener("click", function (evt) {
 });
 
 contactForm.addEventListener("submit", function (evt) {
-    if (!contactName.value && !contactEmail.value && !contactText.value) {
-        evt.preventDefault();
-    } else {
-        if (isStorageSupport) {
-            localStorage.setItem("email", contactEmail.value);
-        }
-    }
+    evt.preventDefault();
 });
 
 window.addEventListener("keydown", function (evt) {
